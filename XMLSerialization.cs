d5fcfc7afc24025d1ca6c0eb09521ccbf34446ac
MaximMazur12 <1mazurmaxim12@gmail.com>
2023-04-25 18:20:45 +0300

    Додав серіалізацію для класу

---
+++ XMLSerialization.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Serialization
+{
+    internal class XMLSeraializer
+    {
+        private XmlSerializer serializer = new XmlSerializer(typeof(ElectricalDevices));
+        public string Serialize(ElectricalDevices devices)
+        {
+            var writer = new StringWriter();
+            serializer.Serialize(writer, devices);
+            var xmlStr = writer.ToString();
+            Console.WriteLine(xmlStr);
+            return xmlStr;
+        }
+          
+    }
+}
