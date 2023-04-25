39506be81818f2b2fbed9f50c6defcb2d6ffab82
MaximMazur12 <1mazurmaxim12@gmail.com>
2023-04-25 19:16:21 +0300

    додав JSON серіалізацію\десеріалізацію

---
+++ JSONSerialized.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace JSON_HW12
+{
+   public class JSONSerialized
+    {
+        public string Serialize(ElectricalDevices electrDev)
+        {
+            var json = JsonConvert.SerializeObject(electrDev);
+            Console.WriteLine(json);
+            return json;
+        }
+
+        public ElectricalDevices Deserialize(string json)
+        {
+            var deserializeDev = JsonConvert.DeserializeObject<ElectricalDevices>(json);
+            return deserializeDev; 
+        }
+    }
+}
