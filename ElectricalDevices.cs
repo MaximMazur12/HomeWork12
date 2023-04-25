5e95ba889db102c109480df35581a277438743f8
MaximMazur12 <1mazurmaxim12@gmail.com>
2023-04-25 18:18:49 +0300

    Додав клас з 2 ДЗ для серіалізації

---
+++ ElectricalDevices.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using System.Text.Json.Serialization;
+
+namespace Serialization
+{
+    [Serializable]
+    public  class ElectricalDevices
+    {
+        protected string Name { get; set; }
+        public int dateRealise;
+        protected int weight;
+        protected string operatingSystem;
+        private decimal Price { get; }
+
+        public virtual void Sum(int a, int b)      
+        {
+            int Sum = a + b;
+        }
+
+        public float MathOperations(float a, float b)   
+        {
+            return a + b;
+        }
+        public ElectricalDevices()
+        {
+            Name = "Samsung";
+            dateRealise = 2010;
+            operatingSystem = "Android";
+        }
+    }
+}
